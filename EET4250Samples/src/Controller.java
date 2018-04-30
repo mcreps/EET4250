ecf9d84e307b608af4a5c860921cacce1d0c510f
Merl J Creps Jr <merl.crepsjr@bradyplc.com>
2018-04-30 18:14:46 -0400

    Initial Commit

---
+++ EET4250Samples/src/Controller.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.connection.DatabaseManager;
+
+public class Controller {
+
+	private static Logger logger = LoggerFactory.getLogger(Controller.class);
+	
+	public static void main(String[] args) {
+		
+		/* Make a database connections with properties files */
+		DatabaseManager databaseManager = new DatabaseManager("database.properties");
+		Connection connection = databaseManager.establishConnection();
+		if (null == connection) {
+			System.exit(1);
+		}
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		/* Close the database connection */
+		databaseManager.closeConnection(connection);
+		scanner.close();
+		logger.debug("Program completed");
+	}
+
+}
